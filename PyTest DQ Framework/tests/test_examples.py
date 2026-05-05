ff6aff44be85c20b3d9ebe50e4955e5e347d24b5
Sofiia <sophiazinuk@gmail.com>
2026-05-05 23:55:43 +0300

    added mark

--- PyTest DQ Framework/tests/test_examples.py
+++ PyTest DQ Framework/tests/test_examples.py
@@ -12,15 +12,15 @@ def source_data(db_connection):
 @pytest.fixture(scope='module')
 def target_data(parquet_reader):
     target_path = 'parquet_data'
-    target_data = parquet_reader.read_parquet(target_path)
+    target_data = parquet_reader.readagain_parquet(target_path)
     return target_data
 
 
-@pytest.mark.example
+@pytest.mark.parquet_data
 def test_check_dataset_is_not_empty(target_data, data_quality_library):
     data_quality_library.check_dataset_is_not_empty(target_data)
 
 
-@pytest.mark.example
+@pytest.mark.parquet_data
 def test_check_count(source_data, target_data, data_quality_library):
     data_quality_library.check_count(source_data, target_data)
