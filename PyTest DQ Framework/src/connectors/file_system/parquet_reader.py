743e4b541c9f9e93e6bc84f69a3e57e4c671ee1f
Sofiia <sophiazinuk@gmail.com>
2026-05-08 13:04:41 +0300

    fixes on comment

--- PyTest DQ Framework/src/connectors/file_system/parquet_reader.py
+++ PyTest DQ Framework/src/connectors/file_system/parquet_reader.py
@@ -1,19 +1,22 @@
 import pandas as pd
 import os
 
+
 class ParquetReader:
 
     def read_parquet(self, base_path: str):
-        dataframes = []
+
+        parquet_data = {}
 
         for root, dirs, files in os.walk(base_path):
             for file in files:
                 if file.endswith(".parquet"):
                     full_path = os.path.join(root, file)
                     df = pd.read_parquet(full_path)
-                    dataframes.append(df)
+                    file_name = os.path.splitext(file)[0]
+                    parquet_data[file_name] = df
 
-        if not dataframes:
+        if not parquet_data:
             raise ValueError("No parquet files found")
 
-        return pd.concat(dataframes, ignore_index=True)
+        return parquet_data
--- PyTest DQ Framework/tests/test_examples.py
+++ PyTest DQ Framework/tests/test_examples.py
@@ -4,25 +4,46 @@ import os
 
 @pytest.fixture(scope='module')
 def source_data(db_connection):
-    source_query = """
-    SELECT * FROM users
-    """
-    return db_connection.get_data_sql(source_query)
 
+    source_tables = {
+        "facilities": """
+            SELECT * FROM src_generated_facilities
+        """,
+
+        "patients": """
+            SELECT * FROM src_generated_patients
+        """,
+
+        "visits": """
+            SELECT * FROM src_generated_visits
+        """
+    }
+
+    source_dataframes = {}
+
+    for table_name, query in source_tables.items():
+
+        source_dataframes[table_name] = db_connection.get_data_sql(query)
+
+    return source_dataframes
+
+ENTITIES = ["facilities", "patients", "visits"]
 BASE_DIR = os.path.dirname(os.path.dirname(__file__)) 
 
 @pytest.fixture(scope='module')
-def target_data(parquet_reader):
+def parquet_data(parquet_reader):
     target_path = os.path.join(BASE_DIR, "parquet_data")
-    target_data = parquet_reader.read_parquet(target_path)
-    return target_data
+    parquet_data = parquet_reader.read_parquet(target_path)
+    return parquet_data
 
 
-@pytest.mark.parquet_data
-def test_check_dataset_is_not_empty(target_data, data_quality_library):
-    data_quality_library.check_dataset_is_not_empty(target_data)
+@pytest.mark.parametrize("entity", ENTITIES)
+def test_row_count(source_data, parquet_data, entity):
 
+    source_df = source_data[entity]
+    parquet_df = parquet_data[entity]
 
-@pytest.mark.parquet_data
-def test_check_count(source_data, target_data, data_quality_library):
-    data_quality_library.check_count(source_data, target_data)
+    assert len(source_df) == len(parquet_df), (
+        f"Row count mismatch for {entity}: "
+        f"source={len(source_df)}, parquet={len(parquet_df)}"
+    )
